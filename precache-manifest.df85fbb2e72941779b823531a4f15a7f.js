b6cf100685273f2e28113abd505a01852b4cef92
Iurii Gagarin <gagarinbefree@gmail.com>
2019-09-19 18:40:02 +0500

    Updates

---
+++ precache-manifest.df85fbb2e72941779b823531a4f15a7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55dfc3db909259a49f9f03399ef826fb",
+    "url": "/mosing/index.html"
+  },
+  {
+    "revision": "03e894898de46c1c6fcd",
+    "url": "/mosing/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "e55853b81b1923906c62",
+    "url": "/mosing/static/css/main.25f98e03.chunk.css"
+  },
+  {
+    "revision": "03e894898de46c1c6fcd",
+    "url": "/mosing/static/js/2.e6860903.chunk.js"
+  },
+  {
+    "revision": "e55853b81b1923906c62",
+    "url": "/mosing/static/js/main.792a2cdb.chunk.js"
+  },
+  {
+    "revision": "4819f47c1d8db1be1d5d",
+    "url": "/mosing/static/js/runtime~main.ba5ea759.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mosing/precache-manifest.82a878d5cf17782b2e63915baab91867.js"
+  "/mosing/precache-manifest.df85fbb2e72941779b823531a4f15a7f.js"
 );
 
 self.addEventListener('message', (event) => {
